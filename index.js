cb35489d159a62d7385ded56c64feb7bec9c7509
Adames1 <argenisadamesgarcia@gmail.com>
2024-05-07 15:32:32 -0400

    Actualizacion de alerta

--- index.js
+++ index.js
@@ -49,3 +49,22 @@ buttonBg.addEventListener("click", () => {
     });
   });
 });
+
+// alerta para indicar que la app esta en desarrollo
+document.addEventListener("DOMContentLoaded", () => {
+  let reminder = `
+  <div class="reminder">
+    <h2>AVISO IMPORTANTE</h2>
+    <span>Esta aplicación se encuentra en desarrollo, pero para mostrar un poco mis habilidades ha sido agregada al portafolio. Disculpe los inconvenientes.</span>
+    <button class="buttons">De acuerdo</button>
+  </div>`;
+
+  let body = document.querySelector("body");
+  body.insertAdjacentHTML("beforeend", reminder);
+
+  // remover notificacion con click en boton
+  let button = document.querySelector(".reminder button");
+  button.addEventListener("click", () => {
+    button.parentElement.remove();
+  });
+});
